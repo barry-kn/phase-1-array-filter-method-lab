4786d0fc83bd5d4462fb26de4cf4239bf086a137
barry-kn <carrybarryklein49@gmail.com>
2023-03-22 01:01:58 +0300

    1st commit

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+const drivers = [
+    { name: 'Bobby', hometown: 'Pittsburgh' },
+    { name: 'Sammy', hometown: 'New York' },
+    { name: 'Sally', hometown: 'Cleveland' },
+    { name: 'Annette', hometown: 'Los Angeles' },
+    { name: 'Bobby', hometown: 'Tampa Bay' }
+  ];
+  
+  function findMatching(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase() === query.toLowerCase());
+  }
+  
+  function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(query.toLowerCase()));
+  }
+  
+  function matchName(drivers, query) {
+    return drivers.filter(driver => driver.name === query);
+  }
+  console.log(findMatching(drivers, 'bobby'));
+console.log(fuzzyMatch(drivers, 's'));
+console.log(matchName(drivers, 'Sally'));
+[ { name: 'Bobby', hometown: 'Pittsburgh' },  { name: 'Bobby', hometown: 'Tampa Bay' } ]
+[ { name: 'Sammy', hometown: 'New York' },  { name: 'Sally', hometown: 'Cleveland' } ]
+[ { name: 'Sally', hometown: 'Cleveland' } ]
